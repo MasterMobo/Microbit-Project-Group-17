2acbe2e648b22c366bd962fa020bbbce9ce4884f
MasterMobo <55976009+MasterMobo@users.noreply.github.com>
2022-12-11 12:55:18 +0700

    Added set threshold functionality

--- Microbit_Project.py
+++ Microbit_Project.py
@@ -6,13 +6,16 @@ currentShow = 1                                             # The pot currently 
 manualMode = False                                          # Whether the system is in manual mode or not
 manualModeTimeElapsed = 0                                   # How many seconds the system has been in manual mode (refreshes after any user input)
 manualModeTimeWindow = 10                                   # How many seconds the system can be in manual mode
+settingThreshold = False
+MAX_VAL = 800
+MIN_VAL = 300
 THRESHOLD = 430                                             # The value after which the valve will open
 
 # Initialize I2C adress of LCD
 makerbit.connect_lcd(39)
 makerbit.clear_lcd1602()
 
-# Custom characters for LCD display
+# Custom characters for LCD display 
 makerbit.lcd_make_character(LcdChar.C1,
     makerbit.lcd_character_pixels("""
         # # # # #
@@ -95,16 +98,19 @@ def on_forever():
     # Update values of sensors
     updatePins()
 
-    if manualMode == False:
-        for i in range(len(sensors)):
-            # Check moisture, if too low then open valve
-            if sensorVal[i] >= THRESHOLD:
-                openValve(i)
-            else:
-                closeValve(i)
+    if settingThreshold == True:
+        setThresholdWindow()
+    else:
+        if manualMode == False:
+            for i in range(len(sensors)):
+                # Check moisture, if too low then open valve
+                if sensorVal[i] >= THRESHOLD:
+                    openValve(i)
+                else:
+                    closeValve(i)
 
-    #LCD Control
-    showStats(currentShow)
+        #LCD Control
+        showStats(currentShow)
 
 basic.forever(on_forever)
 
@@ -115,10 +121,19 @@ basic.forever(on_forever)
 def updatePins():
     for i in range(len(sensors)):
         sensorVal[i] = pins.analog_read_pin(sensors[i])
-        
+
+def calculatePercent(val):
+    return Math.ceil(Math.map(val, MAX_VAL, MIN_VAL, 0, 100))
+
+def setThresholdWindow():
+    makerbit.clear_lcd1602()
+    makerbit.show_string_on_lcd1602("Set Threshold:", makerbit.position1602(LcdPosition1602.POS1), 16)
+    makerbit.show_string_on_lcd1602(str(THRESHOLD) + " (" + str(calculatePercent(THRESHOLD)) + "%)", makerbit.position1602(LcdPosition1602.POS17), 16)
+
 def showStatsDetailed(showNum):
     global manualMode
-    percent_val = Math.ceil(Math.map(sensorVal[showNum-1], 800, 300, 0, 100))
+
+    percent_val = calculatePercent(sensorVal[showNum-1])
     makerbit.show_string_on_lcd1602("P" + str(showNum), makerbit.position1602(LcdPosition1602.POS1), 2)
     makerbit.show_string_on_lcd1602("" + str(sensorVal[showNum-1]),
         makerbit.position1602(LcdPosition1602.POS4),
@@ -136,11 +151,12 @@ def showStatsDetailed(showNum):
 
 def showStats(showNum):
     global manualMode
-    percent_val = Math.ceil(Math.map(sensorVal[showNum-1], 800, 300, 0, 100))
-    makerbit.show_string_on_lcd1602("Pot" + str(showNum) + ":", makerbit.position1602(LcdPosition1602.POS1), 5)
-    makerbit.show_string_on_lcd1602(str(percent_val) + "%",
+
+    percent_val = calculatePercent(sensorVal[showNum-1])
+    makerbit.show_string_on_lcd1602("Pot" + str(showNum) + ": ", makerbit.position1602(LcdPosition1602.POS1), 6)
+    makerbit.show_string_on_lcd1602(str(percent_val) + "% ",
         makerbit.position1602(LcdPosition1602.POS7),
-        3)
+        4)
     progressBar(percent_val)
     if manualMode == True:
         makerbit.show_string_on_lcd1602("Manual", makerbit.position1602(LcdPosition1602.POS11), 6)
@@ -174,27 +190,42 @@ def openValve(ind: number):
     pins.digital_write_pin(valves[ind], 1)
 
 
-def changeCurrentShow():
+def showNext():
     if manualMode == False:
         global currentShow
         currentShow += 1
         if currentShow > 3:
             currentShow = 1
-loops.every_interval(5000, changeCurrentShow)
+loops.every_interval(5000, showNext)
 
-#--Manual-mode-control------------------------------------------------------------
+def startManualMode():
+    global manualMode
+    for i in range(len(sensors)):
+        closeValve(i)
+    manualMode = True
+
+#--Button-control------------------------------------------------------------
 
 def on_button_pressed_a():
     global currentShow
     global manualMode
     global manualModeTimeElapsed
+    global THRESHOLD
+    global MIN_VAL
+
+    if settingThreshold:
+        THRESHOLD = THRESHOLD - 5
+        if THRESHOLD < MIN_VAL:
+            THRESHOLD = MIN_VAL
+        return
+
     if manualMode == True:
         currentShow -= 1
         if currentShow < 1:
             currentShow = 3
         manualModeTimeElapsed = 0
     else:
-        manualMode = True
+        startManualMode()
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 
@@ -202,13 +233,22 @@ def on_button_pressed_b():
     global currentShow
     global manualMode
     global manualModeTimeElapsed
+    global THRESHOLD
+    global MAX_VAL
+
+    if settingThreshold:
+        THRESHOLD = THRESHOLD + 5
+        if THRESHOLD > MAX_VAL:
+            THRESHOLD = MAX_VAL
+        return
+
     if manualMode == True:
         currentShow += 1
         if currentShow > 3:
             currentShow = 1
         manualModeTimeElapsed = 0
     else:
-        manualMode = True
+        startManualMode()
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 
@@ -216,11 +256,19 @@ def on_button_pressed_ab():
     global currentShow
     global manualMode
     global manualModeTimeElapsed
+    global settingThreshold
+
     if manualMode == True:
         openValve(currentShow - 1)
         basic.pause(2000)
         closeValve(currentShow - 1)
         manualModeTimeElapsed = 0
+    else:
+        if settingThreshold == False:
+            settingThreshold = True
+        else:
+            settingThreshold = False
+
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 
